3f364fb27c5b6a61df30a74d1ae7c0ddae7b615c
Rafael Carvalho <rafacarv2@gmail.com>
2018-12-05 00:26:29 -0200

    Implementando menu principal

---
+++ JumpGuy/Assets/Scripts/Spinner.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour {
+
+	// Use this for initialization
+	[SerializeField]
+	private float multiplicador = 1;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.up * Time.deltaTime*multiplicador, Space.World);
+	}
+}
